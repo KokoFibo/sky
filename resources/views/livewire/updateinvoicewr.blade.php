9f3e304a9b104b8aaca81ab81218e61b3b7de131
Anton <kokonacci@gmail.com>
2023-03-30 14:53:55 +0700

    masih bingung dgn tax2

--- resources/views/livewire/updateinvoicewr.blade.php
+++ resources/views/livewire/updateinvoicewr.blade.php
@@ -87,6 +87,20 @@
                     </select>
                 </div>
 
+                <div class="flex">
+                    <span
+                        class="inline-flex items-center w-24 px-3 text-sm text-gray-900 bg-gray-200 border border-r-0 border-gray-300 rounded-l-md dark:bg-gray-600 dark:text-gray-400 dark:border-gray-600">
+
+                        {{-- class="inline-flex items-center w-32 px-3 text-sm text-gray-900 bg-gray-200 border border-r-0 border-gray-300 rounded-l-md dark:bg-gray-600 dark:text-gray-400 dark:border-gray-600"> --}}
+                        Tax
+                    </span>
+
+                    <input id="tax" type="text" name="tax" :value="old('tax')" required wire:model="tax"
+                        autocomplete="tax" {{ $updateUpper ? '' : 'disabled' }}
+                        class="w-full rounded-none rounded-r-lg bg-gray-50 border text-gray-900 focus:ring-blue-500 focus:border-blue-500 block flex-1 min-w-0 text-sm border-gray-300 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+
+                </div>
+
 
             </div>
 
