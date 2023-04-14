33e9015122e4d8330c5b18e879277d34e7bfa76f
Anton <kokonacci@gmail.com>
2023-04-14 20:49:06 +0700

    detail mobile done

--- resources/views/modal/detailQuotationModal.blade.php
+++ resources/views/modal/detailQuotationModal.blade.php
@@ -1,10 +1,10 @@
-<div x-show="openModal" mx-auto x-cloak @click="openModal = false">
-    <div class="w-3/4 mx-auto mt-3 text-black bg-white shadow rounded-xl border-1">
+<div class="p-3" x-show="openModal" mx-auto x-cloak @click="openModal = false">
+    <div class="w-full mx-auto mt-3 text-black bg-white shadow lg:w-3/4 rounded-xl border-1">
         <h2 class="py-3 text-2xl font-semibold text-center">Detail Quotation</h2>
     </div>
 
-    <div class="flex w-3/4 gap-10 p-5 mx-auto">
-        <div class="w-1/2">
+    <div class="flex flex-col w-full p-5 mx-auto lg:gap-10 lg:flex-row lg:w-3/4">
+        <div class="w-full lg:w-1/2">
             <div class="flex mt-3 ">
                 <span
                     class="inline-flex items-center w-32 px-3 text-sm text-gray-900 bg-gray-200 border border-r-0 border-gray-300 rounded-l-md dark:bg-gray-600 dark:text-gray-400 dark:border-gray-600">
@@ -38,7 +38,7 @@
 
 
         </div>
-        <div class="w-1/2">
+        <div class="w-full lg:w-1/2">
             <div class="flex mt-3 ">
                 <span
                     class="inline-flex items-center w-32 px-3 text-sm text-gray-900 bg-gray-200 border border-r-0 border-gray-300 rounded-l-md dark:bg-gray-600 dark:text-gray-400 dark:border-gray-600">
@@ -65,48 +65,47 @@
 
         </div>
     </div>
+    <div class="w-full py-4 overflow-x-auto">
+        <table
+            class="w-full mx-auto mt-3 text-sm text-left text-gray-500 table-fixed lg:w-3/4 lg:table-auto dark:text-gray-400">
+            <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                <tr class="text-white bg-gray-500 dark:text-white">
 
-    <table class="w-3/4 mx-auto mt-3 text-sm text-left text-gray-500 dark:text-gray-400">
-        <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
-            <tr class="text-white bg-gray-500 dark:text-white">
-                <th scope="col" class="px-6 py-3">#</th>
-                <th scope="col" class="px-6 py-3">Package</th>
-                <th scope="col" class="px-6 py-3">Price</th>
-                <th scope="col" class="px-6 py-3">Description</th>
-            </tr>
-        </thead>
-        <tbody>
-            @php
-                $subtotal = 0;
-            @endphp
-            @foreach ($detailQuotations as $d)
-                <tr class="border-b dark:bg-gray-800 dark:border-gray-700 even:bg-gray-200 hover:bg-blue-200">
-                    <td class="px-6 py-4"> {{ $loop->iteration }}</td>
-                    <td class="px-6 py-4"> {{ $d->package }}</td>
-                    <td class="px-6 py-4"> {{ number_format($d->price) }}</td>
-                    <td class="px-6 py-4">
-                        @php
-                                    $desc = getDetail($d->description);
-                                @endphp
-                                    <ul>
-                                        @php
-                                        foreach ($desc as $de) {
-                                            @endphp
-                                            <li class="list-disc">
-                                                @php
-                                                echo $de;
-                                                @endphp
-                                            </li>
-                                            @php
-                                        }
-                                        @endphp
-
-                                    </ul>
-                    </td>
+                    <th class="w-10 px-6 py-3">#</th>
+                    <th class="px-6 py-3 w-80">Package</th>
+                    <th class="px-6 py-3 w-28">Price</th>
+                    <th class="px-6 py-3 w-96">Description</th>
                 </tr>
-            @endforeach
-        </tbody>
-    </table>
+            </thead>
+            <tbody>
+                @php
+                    $subtotal = 0;
+                @endphp
+                @foreach ($detailQuotations as $d)
+                    <tr class="border-b dark:bg-gray-800 dark:border-gray-700 even:bg-gray-200 hover:bg-blue-200">
+                        <td class="px-6 py-4"> {{ $loop->iteration }}</td>
+                        <td class="px-6 py-4"> {{ $d->package }}</td>
+                        <td class="px-6 py-4 text-right"> {{ number_format($d->price) }}</td>
+                        <td class="px-6 py-4">
+                            @php
+                                $desc = getDetail($d->description);
+                            @endphp
+                            <ul>
+
+                                @foreach ($desc as $de)
+                                    <li class="list-disc">
+                                        {{ $de }}
+                                    </li>
+                                @endforeach
+
+
+                            </ul>
+                        </td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+    </div>
 
 
     <div class="w-3/4 mx-auto mt-44">
