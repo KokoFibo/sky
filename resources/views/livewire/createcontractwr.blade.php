3fc1dbdf05872db71ac8a429d75cd6f4acb26aca
Anton <kokonacci@gmail.com>
2023-04-14 17:50:54 +0700

    contract mobile done

--- resources/views/livewire/createcontractwr.blade.php
+++ resources/views/livewire/createcontractwr.blade.php
@@ -1,10 +1,10 @@
-<div>
-    <div class="w-3/4 mx-auto mt-3 text-black bg-white shadow rounded-xl border-1">
+<div class="p-3">
+    <div class="w-full mx-auto mt-3 text-black bg-white shadow lg:w-3/4 rounded-xl border-1">
         <h2 class="py-3 text-2xl font-semibold text-center">Create Contract</h2>
     </div>
-    <div class="w-3/4 p-5 mx-auto mt-3 text-black bg-white shadow rounded-xl border-1">
-        <div class="flex items-start justify-between w-full px-10 ">
-            <div class="flex flex-col w-1/3 gap-3">
+    <div class="w-full p-5 mx-auto mt-3 text-black bg-white shadow lg:w-3/4 rounded-xl border-1">
+        <div class="flex flex-col items-start justify-between w-full gap-3 lg:flex-row ">
+            <div class="flex flex-col w-full gap-3 lg:w-1/3">
                 {{-- customer_id --}}
                 <div class="flex ">
                     <span
@@ -35,7 +35,7 @@
                 </div>
 
             </div>
-            <div class="flex flex-col w-1/3 gap-3">
+            <div class="flex flex-col w-full gap-3 lg:w-1/3">
                 <div class="flex ">
                     <span
                         class="inline-flex items-center w-32 px-3 text-sm text-gray-900 bg-gray-200 border border-r-0 border-gray-300 rounded-l-md dark:bg-gray-600 dark:text-gray-400 dark:border-gray-600">
@@ -71,16 +71,17 @@
         </div>
     </div>
     {{-- table --}}
-    <div>
-        <table class="w-3/4 mx-auto mt-3 text-sm text-left text-gray-500 dark:text-gray-400">
+    <div class="w-full py-4 overflow-x-auto">
+        <table
+            class="w-full mx-auto mt-3 text-sm text-left text-gray-500 table-fixed lg:w-3/4 lg:table-auto dark:text-gray-400">
             <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
                 <tr class="text-white bg-gray-500 dark:text-white">
-                    <th scope="col" class="px-6 py-3">#</th>
-                    <th scope="col" class="px-6 py-3">Package</th>
-                    <th scope="col" class="px-6 py-3">Price</th>
-                    <th scope="col" class="px-6 py-3">Qty</th>
-                    <th scope="col" class="px-6 py-3">Description</th>
-                    <th scope="col" class="px-6 py-3">
+                    <th class="w-10 px-6 py-3">#</th>
+                    <th class="px-6 py-3 w-80">Package</th>
+                    <th class="px-6 py-3 w-60">Price</th>
+                    <th class="px-6 py-3 w-60">Qty</th>
+                    <th class="px-6 py-3 w-96">Description</th>
+                    <th class="w-20 px-6 py-3">
                         @if ($customer_id == '' || $contract_begin == null || $contract_end == null)
                             <button wire:click="add_row" class="button button-gray" disabled>Add</button>
                         @else
@@ -95,7 +96,7 @@
                         <tr x-data="{ packageManual: false }" @dblclick="packageManual = !packageManual"
                             class="border-b dark:bg-gray-800 dark:border-gray-700 even:bg-gray-200 hover:bg-blue-200">
                             <td class="px-6 py-4">{{ $loop->iteration }}</td>
-                            <td class="p-3 ">
+                            <td class="px-6 py-4 ">
                                 {{-- <td class="px-6 py-4"> --}}
                                 {{-- <div class="p-3 " x-data="{ packageManual: false }" @dblclick="packageManual = !packageManual "> --}}
                                 <div class="w-full " x-show="!packageManual">
@@ -122,7 +123,7 @@
                             </td>
                             <td class="px-6 py-4">
                                 {{-- input price --}}
-                                <div class="p-3 ">
+                                <div>
                                     <x-text-input class="block w-full mt-1 text-right" type="text" name="price"
                                         onchange="Calc(this);" :value="old('price')"
                                         wire:model.lazy="contracts.{{ $index }}.price" autocomplete="price" />
@@ -132,7 +133,7 @@
                             </td>
                             <td class="px-6 py-4">
                                 {{-- input Qty --}}
-                                <div class="p-3 ">
+                                <div>
                                     <x-text-input class="block w-full mt-1 text-right" type="number" name="qty"
                                         onchange="Calc(this);" :value="old('qty')" required
                                         wire:model.lazy="contracts.{{ $index }}.qty" autocomplete="qty" />
@@ -160,22 +161,21 @@
                 @endif
             </tbody>
         </table>
-
-
-        <div class="flex justify-between w-3/4 mx-auto my-3 ">
-            <div></div>
-            <div class="flex gap-2">
-                <x-blue-button wire:click="saveContract">
-                    {{ __('Save') }}
-                </x-blue-button>
-                <a href="/contract">
-                    <x-primary-button>
-                        {{ __('Cancel') }}
-                    </x-primary-button>
-                </a>
-            </div>
+    </div>
+    <div class="flex justify-between w-full my-3 lg:w-3/4 ">
+        <div></div>
+        <div class="flex gap-2">
+            <x-blue-button wire:click="saveContract">
+                {{ __('Save') }}
+            </x-blue-button>
+            <a href="/contract">
+                <x-primary-button>
+                    {{ __('Back') }}
+                </x-primary-button>
+            </a>
         </div>
     </div>
+
 
 
 
--- resources/views/livewire/updatedetailcontractwr.blade.php
+++ resources/views/livewire/updatedetailcontractwr.blade.php
@@ -1,19 +1,19 @@
-<div>
-    <div class="flex flex-col w-1/3 gap-3 p-3 mx-auto mt-5 text-center bg-white rounded-lg shadow">
+<div class="p-3">
+    <div class="flex flex-col w-full gap-3 p-3 mx-auto mt-5 text-center bg-white rounded-lg shadow lg:w-1/3">
         Edit Contract Item
     </div>
 
-    <div class="flex flex-col w-1/3 gap-3 p-3 mx-auto mt-5 bg-white rounded-lg shadow" x-data="{ inputManual: false }">
+    <div class="flex flex-col w-full gap-3 p-3 mx-auto mt-5 bg-white rounded-lg shadow lg:w-1/3" x-data="{ inputManual: false }">
         {{-- Package --}}
         <div class="flex">
             <span @dblclick="inputManual = !inputManual"
                 class="inline-flex items-center w-24 px-3 text-sm text-gray-900 bg-gray-200 border border-r-0 border-gray-300 rounded-l-md dark:bg-gray-600 dark:text-gray-400 dark:border-gray-600">
                 Package
             </span>
-            <div x-show="inputManual">
+            <div x-show="inputManual" class="w-full">
 
                 <select wire:model="package"
-                    class="w-80 rounded-none rounded-r-lg bg-gray-50 border  border-gray-300 text-gray-600
+                    class="w-full rounded-none rounded-r-lg bg-gray-50 border  border-gray-300 text-gray-600
                 text-sm focus:ring-blue-500 focus:border-blue-500 lg:block p-2.5 dark:bg-gray-700 dark:border-gray-600
                 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
 
@@ -25,10 +25,10 @@
 
                 </select>
             </div>
-            <div x-show="!inputManual" @dblclick="inputManual = !inputManual">
+            <div x-show="!inputManual" @dblclick="inputManual = !inputManual" class="w-full">
 
                 <input type="text" required wire:model="package"
-                    class="w-80 rounded-none rounded-r-lg bg-gray-50 border text-gray-900 focus:ring-blue-500 focus:border-blue-500 block flex-1 min-w-0 text-sm border-gray-300 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                    class="w-full rounded-none rounded-r-lg bg-gray-50 border text-gray-900 focus:ring-blue-500 focus:border-blue-500 block flex-1 min-w-0 text-sm border-gray-300 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
             </div>
 
 
