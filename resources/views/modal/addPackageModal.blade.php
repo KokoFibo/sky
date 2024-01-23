63df5bae660f8c69517dfb5716dbc65ef7719554
KokoFibo <kokonacci@gmail.com>
2024-01-23 17:59:20 +0700

    add comma separated edit 1

--- resources/views/modal/addPackageModal.blade.php
+++ resources/views/modal/addPackageModal.blade.php
@@ -35,4 +35,24 @@
 
 
     </x-modalCustom>
+    <script>
+        document.querySelectorAll('input[type-currency="IDR"]').forEach((element) => {
+            element.addEventListener("keyup", function(e) {
+                let cursorPostion = this.selectionStart;
+                let value = parseInt(this.value.replace(/[^,\d]/g, ""));
+                let originalLenght = this.value.length;
+                if (isNaN(value)) {
+                    this.value = "";
+                } else {
+                    this.value = value.toLocaleString("id-ID", {
+                        currency: "IDR",
+                        style: "currency",
+                        minimumFractionDigits: 0,
+                    });
+                    cursorPostion = this.value.length - originalLenght + cursorPostion;
+                    this.setSelectionRange(cursorPostion, cursorPostion);
+                }
+            });
+        });
+    </script>
 </div>
